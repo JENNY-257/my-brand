a70c8ee3d8dd637b0bd4b6b269194e8e75c63752
Patrick M <j.iribagiza2@gmail.com>
2023-01-27 14:32:30 +0200

    work on local storage

--- comment.js
+++ comment.js
@@ -16,10 +16,10 @@ function validateForm(event) {
     // create and insert the error message
     const errorMessage = document.createElement("div");
     errorMessage.classList.add("error-message");
+    errorMessage.style.color = "red";
     errorMessage.innerHTML = "Please enter your name.";
     name.parentNode.insertBefore(errorMessage, name.nextSibling);
     name.classList.add("error");
-    return;
   } else {
     name.classList.remove("error");
   }
@@ -29,14 +29,16 @@ function validateForm(event) {
     // create and insert the error message
     const errorMessage = document.createElement("div");
     errorMessage.classList.add("error-message");
+    errorMessage.style.color = "red";
     errorMessage.innerHTML = "Please enter a message.";
     message.parentNode.insertBefore(errorMessage, message.nextSibling);
     message.classList.add("error");
-    return;
   } else {
     message.classList.remove("error");
   }
 
-  // if no errors, submit the form
-  form.submit();
+  // check if there are no errors
+  if (!document.querySelectorAll(".error").length) {
+    form.submit();
+  }
 }
